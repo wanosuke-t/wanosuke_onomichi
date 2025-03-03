cafe90c4df4b68358fbd07000d7e41cac2e1195e
和ノ助（わのすけ） <wanosuke.t@gmail.com>
2025-03-03 14:20:16 +0900

    Add Swiper.js, step section

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@ $("#js-drawer-button").on("click", function () {
   $(this).toggleClass("is-checked");
   $("#js-drawer").slideToggle();
 });
+
+// スワイパー
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  slidesPerView: "auto",
+  spaceBetween: 10,
+  loop: true,
+  speed: 5000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    768: {
+      spaceBetween: 20,
+    },
+  },
+});
